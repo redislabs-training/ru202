20e0949e09eeda7be66f782b7a2fb0b1923694d8
Simon Prickett <simon@redislabs.com>
2020-08-04 12:13:25 -0700

    Adds decoding of strings.

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,7 @@ import os
 
 redis = Redis(host=os.environ.get("REDIS_HOST", "localhost"),
               port=os.environ.get("REDIS_PORT", 6379),
-              db=0)
+              db=0,
+              decode_responses=True)
 
 print(redis.get("hello"))
